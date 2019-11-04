b92b48ebf7172fd8c7d4837f9c9023e9a43ebfac
Martha <Arnaud>
2019-11-03 19:41:51 -0600

    imagenes categoria

---
+++ EasyCall/EasyCall/PopOverViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PopOverViewController.swift
+//  EasyCall
+//
+//  Created by Martha Arnaud on 11/3/19.
+//  Copyright © 2019 Mm. All rights reserved.
+//
+
+import UIKit
+
+class PopOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     preferredContentSize = CGSize(width: 250, height: 250)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
