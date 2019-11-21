ce9d17c82e0cc3cd1735a602004cf041902586d3
Martha <Arnaud>
2019-11-21 04:17:10 -0600

    pop over creditos

---
+++ EasyCall/EasyCall/PopOverCreditosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PopOverCreditosViewController.swift
+//  EasyCall
+//
+//  Created by Martha Arnaud on 11/21/19.
+//  Copyright © 2019 Mm. All rights reserved.
+//
+
+import UIKit
+
+class PopOverCreditosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+preferredContentSize = CGSize(width: 400, height: 400)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
